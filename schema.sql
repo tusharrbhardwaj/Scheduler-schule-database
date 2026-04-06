268ef297c080982fb7cce04a7b297cfccf7c8432
Tushar Bhadwaj <tusharrbhardwaj@gmail.com>
2026-04-06 14:10:38 +0200

    Added parent tables with time check constraint

---
+++ schema.sql
@@ -0,0 +1,34 @@
+-- Base tables (Parent Tables) with no dependencies.
+
+-- Table "group" to store group_ids of student groups according to which classes are assigned to studetns.
+CREATE TABLE student_groups (
+  group_id BIGSERIAL PRIMARY KEY
+);
+
+
+-- Table "classrooms" to store room related information like id, capacity, and room number.
+CREATE TABLE classrooms (
+  room_id BIGSERIAL PRIMARY KEY,
+  cr_capacity BIGINT NOT NULL,
+  cr_number VARCHAR(20) UNIQUE
+);
+
+
+-- Table "timeslots" to store all timeslots with their ids, start and end_time for reliable assignment of timeslots to rooms and classes.
+CREATE TABLE timeslots (
+  timeslot_id BIGSERIAL PRIMARY KEY,
+  day VARCHAR(100) NOT NULL,
+  start_time TIME NOT NULL,
+  end_time TIME NOT NULL,
+
+  -- check time input to validate incoming start and end time of each slots
+
+  CHECK (start_time < end_time)
+);
+
+
+-- Table "professor" to store professors' details like their ids, and names
+CREATE TABLE professor (
+  prof_id BIGSERIAL PRIMARY KEY,
+  prof_name VARCHAR(255) NOT NULL
+);
